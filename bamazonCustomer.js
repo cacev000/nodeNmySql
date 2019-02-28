e7ee8f5a9ea43f2eec791b79dc188bd6550d3893
cacevedo <cacev000@gmail.com>
2019-02-28 17:31:20 -0500

    commiting chagnes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,14 +24,64 @@ var table = new Table({
     colWidths: [10, 30, 15]
 });
 
-connection.query('SELECT * from products', function(error, results) {
-    if (error) throw error;
+function displayItems() {
+    connection.query('SELECT * from products', function(error, results) {
+        if (error) throw error;
 
-    results.filter(function(items) {
-        table.push(
-            [items.item_id, items.product_name, items.price]
-        );
+        var idArr = [];
+        results.filter(function(items) {
+            table.push(
+                [items.item_id, items.product_name, items.price]
+            );
+
+            idArr.push(items.item_id);
+        });
+
+        console.log(table.toString());
+
+        buyItem(idArr, results);
     });
+}
 
-    console.log(table.toString());
-});
+function buyItem(itemsId, arrItems) {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'itemId',
+        message: 'Type an item ID that you wish to purchase: '
+    }]).then(function(answer) {
+        var idFoundAt = itemsId.indexOf(parseInt(answer.itemId));
+
+        if (idFoundAt >= 0) {
+            console.log('Found Item');
+            askBuyQuantity();
+            results.filter(function(items) {
+                table.push(
+                    [items.item_id, items.product_name, items.price]
+                );
+
+                idArr.push(items.item_id);
+            });
+        } else {
+            console.log('================================\n');
+            console.log('\nInvalid Product Id.\n');
+            console.log('\n================================\n');
+            // code below delays the function call in order to let the user see the Invalid result
+            setTimeout(function() {
+                displayItems();
+            }, 1000);
+        }
+    });
+}
+
+function askBuyQuantity() {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'quantity',
+        message: 'Type an item ID that you wish to purchase: '
+    }]).then(function(answer) {
+
+
+    });
+}
+
+displayItems();
